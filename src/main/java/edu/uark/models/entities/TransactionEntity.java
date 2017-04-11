896acb939b1f7a07f7c1aa87f92f78a4360481f4
arutiaga <arutiaga@uark.edu>
2017-04-11 14:39:39 -0500

    Create TransactionEntity.java

---
+++ src/main/java/edu/uark/models/entities/TransactionEntity.java
@@ -0,0 +1,20 @@
+
+package edu.uark.models.entities;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.commons.lang3.StringUtils;
+
+import edu.uark.dataaccess.entities.BaseEntity;
+
+public class TransactionEntity extends BaseEntity<TransactionEntity> {
+	@Override
+
+  
+  
+}
