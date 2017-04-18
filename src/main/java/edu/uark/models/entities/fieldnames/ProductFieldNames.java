d73ec5a12e70743d753dcaa70e7e0c67001a0636
ss031 <salvadorsanchez11014@yahoo.com>
2017-04-17 21:13:52 -0500

    Update ProductFieldNames.java

--- src/main/java/edu/uark/models/entities/fieldnames/ProductFieldNames.java
+++ src/main/java/edu/uark/models/entities/fieldnames/ProductFieldNames.java
@@ -3,6 +3,7 @@ package edu.uark.models.entities.fieldnames;
 import edu.uark.dataaccess.entities.BaseFieldNames;
 
 public class ProductFieldNames extends BaseFieldNames {
+	public static final String ID = "id";
 	public static final String LOOKUP_CODE = "lookupcode";
 	public static final String QUANTITY = "quantity";
 	public static final String CREATED_ON = "createOn";
