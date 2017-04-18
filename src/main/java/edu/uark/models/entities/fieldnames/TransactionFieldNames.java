c066acff655affbdc7ab4b89a006d7ba00468488
ss031 <salvadorsanchez11014@yahoo.com>
2017-04-17 19:31:53 -0500

    transaction uploaded

---
+++ src/main/java/edu/uark/models/entities/fieldnames/TransactionFieldNames.java
@@ -0,0 +1,13 @@
+package edu.uark.models.entities.fieldnames;
+
+import edu.uark.dataaccess.entities.BaseFieldNames;
+
+public class TransactionFieldNames extends BaseFieldNames {
+	public static final String RECORD_ID = "recordID";
+	public static final String CASHIER_ID = "cashierID";
+	public static final String TOTAL_AMOUNT = "totalAmount";
+	public static final String TRANSACTION_TYPE = "transactionType";
+	public static final String REFERENCE_ID = "referenceID";
+	public static final String TOTAL_AMOUNT = "totalAmount";
+	public static final String CREATEDON = "crearedon";
+}
