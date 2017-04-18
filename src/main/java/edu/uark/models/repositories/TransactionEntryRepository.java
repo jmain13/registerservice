39d795dad569686d80ee01d1ac7e64f9de380b8e
jcbronne <jared.bro96@gmail.com>
2017-04-17 23:46:41 -0500

    Add files via upload

---
+++ src/main/java/edu/uark/models/repositories/TransactionEntryRepository.java
@@ -0,0 +1,44 @@
+package edu.uark.models.repositories;
+
+import java.sql.SQLException;
+
+import edu.uark.dataaccess.repository.BaseRepository;
+import edu.uark.dataaccess.repository.DatabaseTable;
+import edu.uark.dataaccess.repository.helpers.PostgreFunctionType;
+import edu.uark.dataaccess.repository.helpers.SQLComparisonType;
+import edu.uark.dataaccess.repository.helpers.where.WhereClause;
+import edu.uark.dataaccess.repository.helpers.where.WhereContainer;
+import edu.uark.models.entities.TransactionEntryEntity;
+import edu.uark.models.entities.fieldnames.TransactionEntryFieldNames;
+import edu.uark.models.repositories.interfaces.TransactionEntryRepositoryInterface;
+
+public class TransactionEntryRepository extends BaseRepository<TransactionEntryEntity> implements TransactionEntryRepositoryInterface {
+	@Override
+	public TransactionEntryEntity byLookupCode(String lookupCode) {
+		return this.firstOrDefaultWhere(
+			new WhereContainer(
+				(new WhereClause()).
+					postgreFunction(PostgreFunctionType.LOWER).
+					table(this.primaryTable).
+					fieldName(TransactionEntryFieldNames.PLU).
+					comparison(SQLComparisonType.EQUALS)
+			),
+			(ps) -> {
+				try {
+					ps.setObject(1, lookupCode.toLowerCase());
+				} catch (SQLException e) {}
+
+				return ps;
+			}
+		);
+	}
+	
+	@Override
+	public TransactionEntryEntity createOne() {
+		return new TransactionEntryEntity();
+	}
+	
+	public TransactionEntryRepository() {
+		super(DatabaseTable.PRODUCT);
+	}
+}
