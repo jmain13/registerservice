83b803e341970a3014fec50dd2596a08d1f7723a
Jonathan Main <jhmain356@gmail.com>
2017-04-28 14:45:51 -0500

    Changed createOn to createdon in ProductFieldNames.java

--- src/main/java/edu/uark/models/entities/fieldnames/ProductFieldNames.java
+++ src/main/java/edu/uark/models/entities/fieldnames/ProductFieldNames.java
@@ -6,7 +6,7 @@ public class ProductFieldNames extends BaseFieldNames {
 	public static final String ID = "id";
 	public static final String LOOKUP_CODE = "lookupcode";
 	public static final String QUANTITY = "quantity";
-	public static final String CREATED_ON = "createOn";
+	public static final String CREATED_ON = "createdon";
 	public static final String PRICE = "price";
 	public static final String ACTIVE = "active";
 }
